9ae8c3e3734fb9db5258023111e4666b53ea4b77
Fusiello <riccardo.fusiello@unitn.it>
2023-09-28 17:00:54 +0200

    Lezione 3 aggiornata da finire

--- Lezione 3/main_a.cpp
+++ Lezione 3/main_a.cpp
@@ -48,15 +48,57 @@ class A{
 			cout << "Operator = eseguito A" << endl << endl;
 			return *this;
 		}
-		A operator + (const A& a){
+		
+		A operator + (const A& a) const{
 			//dato += a.dato; dato = dato + a.dato
 			// a1 + a2 non modifica ne a1 ne a2 => devo tornare una nuova istanza di A!!!!
 			// sbagliatissimo quello che abbiamo fatto
+			cout << "Operator + eseguito A" << endl << endl;
 			A temp;
 			temp.dato = /*this->*/dato + a.dato;
 			temp.miaCostante = this->miaCostante + a.miaCostante;
 			return temp;
 			// la variabile è locale, a riga 58 muore, per questo devo restituire la copia della variabile!!!!
+		}
+		A& operator += (const A& a){
+			dato += a.dato;
+			miaCostante += a.miaCostante;
+			cout << "Operator += eseguito A" << endl << endl;
+			return *this;
+		}
+		A& operator ++(){
+			++dato;
+			++miaCostante;
+			cout << "Operator ++ prefisso eseguito A" << endl << endl;
+			return *this;
+		}
+		A operator ++(int){
+			/*
+				A temp = *this;
+				dato++; miaCostante++;
+			*/
+			A temp;
+			temp.dato = dato++;
+			temp.miaCostante = miaCostante++;
+			cout << "Operator ++ postfisso eseguito A" << endl << endl;
+			return temp;
+		}
+		A& operator --(){
+			--dato;
+			--miaCostante;
+			cout << "Operator -- prefisso eseguito A" << endl << endl;
+			return *this;
+		}
+		A operator --(int){
+			/*
+				A temp = *this;
+				dato--; miaCostante--;
+			*/
+			A temp;
+			temp.dato = dato--;
+			temp.miaCostante = miaCostante--;
+			cout << "Operator -- postfisso eseguito A" << endl << endl;
+			return temp;
 		}
 		friend ostream& operator << (ostream& os, const A& a);
 };
@@ -83,7 +125,7 @@ int main(int argc, char ** argv){
 	A a1, a2(3), a3(4, 5);
 	
 	cout << a1 << endl << endl;
-	
+	/*
 	boh(a2);
 	
 	a1 = 5; // c'è un membro costante -> non posso fare quest'operazione altrimenti avrebbe fatto la conversione implicita
@@ -91,7 +133,24 @@ int main(int argc, char ** argv){
 	// a1 = a2; -> non posso usare con variabile costante
 	a1 = a2;
 	cout << a1 << endl << endl;
+	
+	// viene chiamato l'operatore = e +
 	a1 = a1 + a2;
 	cout << a1 << endl << endl;
+	// != da + -> prende due istanze mentre += prende solo una
+	a1 += a2; // a1 = a1 + a2
+	// alla luce di ciò, è più veloce l'operatore +=
+	// non viene creata una copia che usa memoria
+	cout << a1 << endl << endl;
+	*/
+	cout << ++a1 << endl << endl; // prefisso -> stampa 1 4
+	cout << a1 << endl << endl; // stampa 1 4
+	cout << a1++ << endl << endl; // postfisso -> stampa 1 4
+	cout << a1 << endl << endl;	// stampa 2 5
+	// costa di più un post incremento che un pre incremento
+	cout << --a1 << endl << endl; // prefisso -> stampa 1 4
+	cout << a1 << endl << endl; // stampa 1 4
+	cout << a1-- << endl << endl; // postfisso -> stampa 1 4
+	cout << a1 << endl << endl;	// stampa 0 3
 	return 0;
 }
